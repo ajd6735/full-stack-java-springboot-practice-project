e479194a8e894240ec4643b5edbfc1766c85e860
ana diru <ajd6735@psu.edu>
2025-06-27 09:05:54 -0400

    adds user in the model package

--- ecomerce/src/main/java/com/quickcart/ecomerce/model/User.java
+++ ecomerce/src/main/java/com/quickcart/ecomerce/model/User.java
@@ -1,5 +1,39 @@
 package com.quickcart.ecomerce.model;
 
 public class User {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public User(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    private double price;
 
 }
