a1d8c8f90e8830de691ead3ea84a9f11a1430e5e
ana diru <ajd6735@psu.edu>
2025-06-27 08:21:46 -0400

    initial commit

---
+++ ecomerce/src/main/java/com/quickcart/ecomerce/model/User.java
@@ -0,0 +1,5 @@
+package com.quickcart.ecomerce.model;
+
+public class User {
+
+}
---
+++ ecomerce/src/test/java/com/quickcart/ecomerce/QuickCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickcart.ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
